5bad112bd31697d4d3d5ec32abdd19bbc927f9c6
aborr <>
2000-01-19 22:10:59 +0000

    Initial import of tls extension.

---
+++ tests/tls.tcl
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 1997-1999 Matt Newman <matt@novadigm.com>
+#
+# $Header$
+#
+set dir [file dirname [info script]]
+regsub {\.} [info tclversion] {} vshort
+if {$tcl_platform(platform) == "windows"} {
+    if {[info exists tcl_platform(debug)]} {
+	load $dir/../win/Debug$vshort/tls.dll
+    } else {
+	load $dir/../win/Release$vshort/tls.dll
+    }
+} else {
+    load [glob $dir/../unix/libtls*]
+}
+
+proc bgerror {err} {
+    global errorInfo
+    puts stderr "BG Error: $errorInfo"
+}
+
+source $dir/../library/tls.tcl
+set tls::debug 2
+
